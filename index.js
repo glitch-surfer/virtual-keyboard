ac38d2be7b2369fce8f1d2082bec747ac96b2f31
vladislav-garaev <marker661@gmail.com>
2023-04-26 13:13:12 +0300

    feat: add generating other pages elements

--- index.js
+++ index.js
@@ -8,16 +8,41 @@ document.addEventListener('keydown', (e) => {
   console.log(JSON.stringify(arr));
 }); */
 
-const container = document.createElement('div');
-container.classList.add('keyboard');
-document.body.append(container);
-
+const generateTitle = () => {
+  const title = document.createElement('h1');
+  title.classList.add('main-title');
+  title.textContent = 'Virtual Keyboard';
+  return title;
+};
+const generateTextArea = () => {
+  const textArea = document.createElement('textarea');
+  textArea.classList.add('text-area');
+  textArea.setAttribute('cols', '100');
+  textArea.setAttribute('rows', '10');
+  return textArea;
+};
+const generateKeyboardContainer = () => {
+  const container = document.createElement('div');
+  container.classList.add('keyboard');
+  return container;
+};
 const generateBtn = (key) => {
   const btn = document.createElement('div');
   btn.className = key.classes;
-  container.append(btn);
   btn.textContent = key.keyEng;
+  return btn;
 };
-for (let i = 0; i < keyboard.length; i += 1) {
-  generateBtn(keyboard[i]);
-}
+const fillKeyboardContainer = () => {
+  const container = document.querySelector('.keyboard')
+  for (let i = 0; i < keyboard.length; i += 1) {
+    container.append(generateBtn(keyboard[i]));
+  }
+};
+
+const generatePage = () => {
+  document.body.append(generateTitle());
+  document.body.append(generateTextArea());
+  document.body.append(generateKeyboardContainer());
+  fillKeyboardContainer();
+};
+generatePage();
