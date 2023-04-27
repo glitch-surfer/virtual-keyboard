14bd2b5387e859838df45b11e2234e4f7c01699a
vladislav-garaev <marker661@gmail.com>
2023-04-27 22:43:49 +0300

    feat: add enter, tab, space, backspace functions

--- modules/insert-phisical-Keyboard.js
+++ modules/insert-phisical-Keyboard.js
@@ -3,14 +3,24 @@ import keyboard from "./keyboard.js";
 const insertPhisicalKeyboard = (event) => {
   const code = event.code;
   const idx = keyboard.findIndex((item) => item.code === code);
+  const textarea = document.querySelector('.text-area');
   let key;
   if (document.currerntLanguage) {
     key = keyboard[idx].keyEng;
   } else {
     key = keyboard[idx].keyRus;
   }
-  const textarea = document.querySelector('.text-area');
-  textarea.textContent += key;
+  if (code === 'Enter') {
+    textarea.value += '\n';
+  } else if (code === 'Backspace') {
+    textarea.value = textarea.value.slice(0, -1);
+  } else if (code === 'Space') {
+    textarea.value += ' ';
+  } else if (code === 'Tab') {
+    textarea.value += '  ';
+  } else {
+    textarea.value += key;
+  }
 };
 
 export default insertPhisicalKeyboard;
--- modules/physical-keyboard-handler.js
+++ modules/physical-keyboard-handler.js
@@ -1,11 +1,11 @@
 import keyboard from "./keyboard.js";
 
 const physicalKeyboardHandler = (event) => {
+  event.preventDefault();
   const key = event.code;
   const idx = keyboard.findIndex((item) => item.code === key);
   const container = document.querySelector('.keyboard');
   const virtualKey = container.children[idx];
-
   if (event.type === 'keydown') {
     if (virtualKey.classList.contains('active') === false) {
       virtualKey.classList.add('active');
