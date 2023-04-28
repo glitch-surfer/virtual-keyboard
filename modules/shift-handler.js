1e47796bb48a6d07df8bf79cefa048e6d71a5ca8
vladislav-garaev <marker661@gmail.com>
2023-04-28 21:54:49 +0300

    feat: add shift for vurtual keyboard

--- modules/shift-handler.js
+++ modules/shift-handler.js
@@ -2,7 +2,9 @@ import keyboard from './keyboard.js';
 
 const shiftDown = (event) => {
   if (event.code === 'ShiftLeft'
-  || event.code === 'ShiftRight') {
+  || event.code === 'ShiftRight'
+  || event.target.classList.contains('shift-right')
+  || event.target.classList.contains('shift-left')) {
     const btn = document.querySelectorAll('.btn');
     btn.forEach((item, index) => {
       const key = item;
@@ -14,10 +16,12 @@ const shiftDown = (event) => {
     });
   }
 };
-
+//  TODO: resolve interaction witch caps lock
 const shiftUp = (event) => {
   if (event.code === 'ShiftLeft'
-  || event.code === 'ShiftRight') {
+  || event.code === 'ShiftRight'
+  || event.target.classList.contains('shift-right')
+  || event.target.classList.contains('shift-left')) {
     const btn = document.querySelectorAll('.btn');
     btn.forEach((item, index) => {
       const key = item;
