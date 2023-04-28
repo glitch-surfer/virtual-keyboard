383c40e4ab778eb5efb57f687f114f18e3133ff1
vladislav-garaev <marker661@gmail.com>
2023-04-28 18:03:05 +0300

    feat: finish insert functions

--- modules/insert-phisical-Keyboard.js
+++ modules/insert-phisical-Keyboard.js
@@ -4,12 +4,8 @@ const insertPhisicalKeyboard = (event) => {
   const code = event.code;
   const idx = keyboard.findIndex((item) => item.code === code);
   const textarea = document.querySelector('.text-area');
-  let key;
-  if (document.currerntLanguage) {
-    key = keyboard[idx].keyEng;
-  } else {
-    key = keyboard[idx].keyRus;
-  }
+  const key = document.querySelector('.keyboard').children[idx].textContent;
+  
   if (code === 'Enter') {
     textarea.value += '\n';
   } else if (code === 'Backspace') {
@@ -18,6 +14,15 @@ const insertPhisicalKeyboard = (event) => {
     textarea.value += ' ';
   } else if (code === 'Tab') {
     textarea.value += '  ';
+  } else if (code === 'Caps Lock'
+  || code === 'ShiftLeft'
+  || code === 'ShiftRight'
+  || code === 'ControlLeft'
+  || code === 'AltLeft'
+  || code === 'MetaLeft'
+  || code === 'MetaRight'
+  || code === 'AltRight') {
+    textarea.value += '';
   } else {
     textarea.value += key;
   }
--- modules/insert-virtual-Keyboard.js
+++ modules/insert-virtual-Keyboard.js
@@ -1,7 +1,24 @@
 const insertVirtualKeyboard = (event) => {
   if (event.target.closest('.btn')) {
-    const key = event.target.innerHTML;
+    const key = event.target.textContent;
     const textarea = document.querySelector('.text-area');
+    if (key === 'Enter') {
+      textarea.value += '\n';
+    } else if (key === 'Backspace') {
+      textarea.value = textarea.value.slice(0, -1);
+    } else if (key === '') {
+      textarea.value += ' ';
+    } else if (key === 'Tab') {
+      textarea.value += '  ';
+    } else if (key === 'Caps Lock'
+    || key === 'Shift'
+    || key === 'Control'
+    || key === 'Option'
+    || key === 'Command') {
+      textarea.value += '';
+    } else {
+      textarea.value += key;
+    }
     textarea.textContent += key;
   }
 };
